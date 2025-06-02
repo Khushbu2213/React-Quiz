35e0de27b49ff89476cbbe53ace76ddf36f9635f
Khushbu <khushbutangar2213@gmail.com>
2025-06-02 22:45:48 +0530

    Initial Commit of React Quiz

---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,28 @@
+function FinishedScreen({ dispatch, points, maxPossiblePoints, highscore }) {
+  const per = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (per === 100) emoji = "🥇";
+  if (per >= 80 && per < 100) emoji = "🥈";
+  if (per >= 50 && per < 80) emoji = "🥉";
+  if (per >= 0 && per < 50) emoji = "🏅";
+  if (per === 0) emoji = "😑";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(per)})%
+      </p>
+      <p className="highscore">Highscore: {highscore} points</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishedScreen;
